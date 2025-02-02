01b85ae16f262d6bed34e2c55c9f9c381f882304
Bob Balooney <firdausfitri010199@gmail.com>
2025-02-03 01:55:16 +0800

    Updated few stuff

---
+++ eks_normal/main.tf
@@ -0,0 +1,71 @@
+# Create AWS EKS Cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "${var.cluster_name}"
+  role_arn = aws_iam_role.eks_master_role.arn
+  version = var.cluster_version
+
+  vpc_config {
+    subnet_ids = data.terraform_remote_state.vpc.outputs.output_vpc_public_subnet
+    endpoint_private_access = var.cluster_endpoint_private_access
+    endpoint_public_access  = var.cluster_endpoint_public_access
+    public_access_cidrs     = var.cluster_endpoint_public_access_cidrs    
+  }
+
+  kubernetes_network_config {
+    service_ipv4_cidr = var.cluster_service_ipv4_cidr
+  }
+  
+  # Enable EKS Cluster Control Plane Logging
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
+  ]
+}
+
+# Create AWS EKS Node Group - Public
+resource "aws_eks_node_group" "eks_ng_public" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+
+  node_group_name = "daus-eks-ng-public"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids      = data.terraform_remote_state.vpc.outputs.output_vpc_public_subnet
+  #version = var.cluster_version #(Optional: Defaults to EKS Cluster Kubernetes version)    
+  
+  ami_type = "AL2_x86_64"  
+  capacity_type = "ON_DEMAND"
+  disk_size = 20
+  instance_types = ["t3.medium"]
+  
+  
+  remote_access {
+    ec2_ssh_key = "daus-terraform"
+  }
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1    
+    max_size     = 2
+  }
+
+  # Desired max percentage of unavailable worker nodes during node group update.
+  update_config {
+    max_unavailable = 1    
+    #max_unavailable_percentage = 50    # ANY ONE TO USE
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-AmazonEC2ContainerRegistryReadOnly,
+  ] 
+
+  tags = {
+    Name = "Public-Node-Group"
+  }
+}
---
+++ eks_normal/version.tf
@@ -0,0 +1,18 @@
+# Terraform Settings Block
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "daus-terraform"
+    key    = "dev/eks-normal/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "eks-normal"
+  }
+}
